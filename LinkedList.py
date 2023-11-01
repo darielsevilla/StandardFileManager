bbf071a0136acbde24b32664668c7bc1832a23ac
Dariel Sevilla <darielsevilla@gmail.com>
2023-11-01 17:53:58 -0600

    ventana crear archivo terminada

---
+++ LinkedList.py
@@ -0,0 +1,116 @@
+class Node:
+    def __init__(self, data):
+        self.previous = None
+        self.data = data
+        self.next = None
+    def __str__(self):
+        return self.data
+
+class LinkedList:
+    def __init__(self):
+        self.last = None
+        self.first = None
+        self.size = 0
+
+    def print(self):
+        currentNode = self.first
+        while(currentNode):
+            print(currentNode.data)
+            currentNode = currentNode.next
+
+    def insertAtEnd(self,node):
+        currentNode = self.last
+        if currentNode is None:
+            self.last = node
+            self.first = node
+            self.size += 1
+        else:
+            node.previous = self.last
+            self.last.next = node
+            self.last = node
+            self.size += 1
+    
+    def insertAtFront(self,node):
+        currentNode = self.first
+        if currentNode is None:
+            self.last = node
+            self.first = node
+            self.size += 1
+        else:
+            node.next = currentNode
+            self.first = node
+            self.size += 1
+
+    def insertAtIndex(self,node,index):
+        if (index > 0 and index <= self.size):
+            if(index == 1):
+                self.insertAtFront(node)
+            elif(index == self.size):
+                self.insertAtEnd(node)
+            else:
+                currentNode = self.first
+                for x in range(1,index):
+                    currentNode = currentNode.next
+                node.next = currentNode
+                node.previous = currentNode.previous
+                currentNode.previous.next = node
+                currentNode.previous = node
+            self.size += 1
+        else:
+            raise IndexError("Index out of bounds")
+        
+    def deleteLast(self):
+        if self.last is None:
+            return
+        else:
+            currentNode = self.last
+            self.last = currentNode.previous
+            currentNode.previous.next = None
+            currentNode = None
+            self.size -= 1
+
+    def deleteFirst(self):
+        if self.first is None:
+            return
+        else:
+            currentNode = self.first
+            self.first = currentNode.next
+            self.first.previous = None
+            currentNode = None
+            self.size -= 1
+    
+    def deleteAtIndex(self,index):
+        if(index > 0 and index <= self.size):
+            if(index == 1):
+                self.deleteFirst()
+            elif(index == self.size):
+                self.deleteLast()
+            else:
+                currentNode = self.first
+                for x in range(1, index):
+                    currentNode = currentNode.next
+                currentNode.previous.next = currentNode.next
+                currentNode.next.previous = currentNode.previous
+                currentNode = None
+                self.size -= 1
+        else:
+            raise IndexError("Index out of bounds")
+    
+    def updateNode(self,newData,index):
+        currentNode = self.first
+        if(index > 0 and index <= self.size):
+            currentNode = self.first
+            for x in range(1,index):
+                currentNode = currentNode.next
+            currentNode.data = newData
+        else:
+            raise IndexError("Index out of bounds")
+
+    def getPrevious(self,node):
+        return node.previous
+    
+    def getNext(self,node):
+        return node.next
+
+    def getSize(self):
+        return self.size
